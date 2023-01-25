ab65d43069df6c450dbd157f038bd85b3c8dc8b1
Maurício Costa <86126994+mauriciomunky@users.noreply.github.com>
2023-01-25 14:28:18 -0300

    Add Book View

--- Bookworm/BookwormApp.swift
+++ Bookworm/BookwormApp.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 @main
 struct BookwormApp: App {
+    @StateObject private var dataController = DataController()
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
 }
