a2392adcae441531e21c2d0751f6285ec1dac346
Maurício Costa <86126994+mauriciomunky@users.noreply.github.com>
2023-01-25 17:04:08 -0300

    Project Complete

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -9,11 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+    @FetchRequest(sortDescriptors: [SortDescriptor(\.title), SortDescriptor(\.author)]) var books: FetchedResults<Book>
     @State private var showingAddScreen = false
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+            try? moc.save()
+        }
+    }
     var body: some View {
         NavigationView {
-            Text("Count: \(books.count)").navigationTitle("Bookworm").toolbar {
+            List {
+                ForEach(books) { book in
+                    NavigationLink {
+                        DetailView(book: book)
+                    } label: {
+                        HStack {
+                            EmojiRatingView(rating: book.rating).font(.largeTitle)
+                            VStack(alignment: .leading) {
+                                Text(book.title ?? "Unknown Title").font(.headline)
+                                Text(book.author ?? "Unknown Author").foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                }.onDelete(perform: deleteBooks)
+            }.navigationTitle("Bookworm").toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
                         showingAddScreen.toggle()
@@ -21,12 +45,13 @@ struct ContentView: View {
                         Label("Add Book", systemImage: "plus")
                     }
                 }
-            }.sheet(isPresented: $showingAddScreen) {
-                AddBookView()
             }
+        }.sheet(isPresented: $showingAddScreen) {
+            AddBookView()
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,32 @@
+//
+//  EmojiRatingDisplay.swift
+//  Bookworm
+//
+//  Created by Maurício Costa on 25/01/23.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int16
+    var body: some View {
+        switch rating {
+        case 1:
+            Text("😫")
+        case 2:
+            Text("☹️")
+        case 3:
+            Text("😐")
+        case 4:
+            Text("🙂")
+        default:
+            Text("😊")
+        }
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiRatingView(rating: 3)
+    }
+}
