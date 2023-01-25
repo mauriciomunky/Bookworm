e286651f9e0ffc5c7ba55ac52c31dedcbbde4735
Maurício Costa <86126994+mauriciomunky@users.noreply.github.com>
2023-01-24 21:11:02 -0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Maurício Costa on 24/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
